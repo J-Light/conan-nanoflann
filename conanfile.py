c1cf5a47ef3f7d4540eb6e2d55d303a7e99c425e
Nishit Joseph <nish.joseph@leapaust.com.au>
2018-06-17 12:35:32 +1000

    Follow bincrafters template

--- conanfile.py
+++ conanfile.py
@@ -6,23 +6,29 @@ from conans import ConanFile, CMake, tools
 class NanoflannConan(ConanFile):
     name = "nanoflann"
     version = "master"
-    license = "BSD"
-    url = "https://github.com/J-Light/conan-nanoflann"
     description = ("nanoflann is a C++11 header-only library for building "
                    + "KD-Trees of datasets with different topologies")
-    settings = "os", "compiler", "build_type", "arch"
+    url = "https://github.com/J-Light/conan-nanoflann"
+    homepage = "https://github.com/jlblancoc/nanoflann"
+
+    license = "BSD"
+
+    exports = ['LICENSE.md']
+
+    exports_sources = ["CMakeLists.txt"]
+    generators = "cmake"
+
+    settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
         "build_benchmarks": [True, False],
         "build_examples": [True, False],
-        "build_tests": [True, False]
-    }
+        "build_tests": [True, False]}
     default_options = (
         "shared=False",
         "build_benchmarks=False ",
         "build_examples=False ",
         "build_tests=False")
-    generators = "cmake"
 
     source_subfolder = "source_subfolder"
     build_subfolder = "build_subfolder"
@@ -32,23 +38,24 @@ class NanoflannConan(ConanFile):
         tools.get("{0}/archive/{1}.tar.gz".format(source_url, self.version))
         extracted_dir = self.name + "-" + self.version
         os.rename(extracted_dir, self.source_subfolder)
-        
+
+    def configure_cmake(self):
+        cmake = CMake(self)
+        cmake.definitions["BUILD_BENCHMARKS"] = self.options.build_benchmarks
+        cmake.definitions["BUILD_EXAMPLES"] = self.options.build_examples
+        cmake.definitions["BUILD_TESTS"] = self.options.build_tests
+        cmake.definitions["BUILD_SHARED_LIBS"] = self.options.shared
+        cmake.configure(build_folder=self.build_subfolder)
+        return cmake
+
     def build(self):
-        # cmake = CMake(self)
-        # cmake.definitions["BUILD_BENCHMARKS"] = self.options.build_benchmarks
-        # cmake.definitions["BUILD_EXAMPLES"] = self.options.build_examples
-        # cmake.definitions["BUILD_TESTS"] = self.options.build_tests
-        # cmake.definitions["BUILD_SHARED_LIBS"] = self.options.shared
-        # cmake.configure(build_folder=self.build_subfolder)
-        # cmake.build()
-        # cmake.install()
-        return
+        cmake = self.configure_cmake()
+        cmake.build()
 
     def package(self):
-        header_dir = os.path.join(self.source_subfolder, "include")
-        self.copy("*.hpp", dst="include", src=header_dir)
+        self.copy(pattern="LICENSE", dst="licenses", src=self.source_subfolder)
+        cmake = self.configure_cmake()
+        cmake.install()
 
     def package_info(self):
         self.info.header_only()
-        # self.cpp_info.libs = ["hello"]
-        return
